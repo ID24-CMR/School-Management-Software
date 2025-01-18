b50d872688cf7af95a3fdda28bc2dbcd6c45ec8e
ID24-CMR <idrice.tsafouet@yahoo.com>
2025-01-18 12:37:16 +0100

    update file
    this is to create the home page (landing page)
    
    Co-Authored-By: idrice24 <50774032+idrice24@users.noreply.github.com>

--- src/main/java/org/idrice24/controllers/LoginController.java
+++ src/main/java/org/idrice24/controllers/LoginController.java
@@ -1,5 +1,6 @@
 package org.idrice24.controllers;
 
+import org.idrice24.entities.Users;
 import org.idrice24.services.UserServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,9 +17,15 @@ public class LoginController {
     }
 
     @GetMapping("/login")
-    public String loginPage(Model model){
-        model.addAttribute("model", model);
+    public String loginPage(Model model, Users users){
+        model.addAttribute("users", users);
         return "login";
+    }
+
+    @GetMapping("/users/signin/{email}")
+    public String signinPage(Model model){
+
+        return "index";
     }
 
 }
--- src/main/java/org/idrice24/services/AppUserRole.java
+++
@@ -1,6 +0,0 @@
-package org.idrice24.services;
-
-public enum AppUserRole {
-    USER,
-    ADMIN
-}
